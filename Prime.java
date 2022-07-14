3fc6c8e09e457738491ac43a1f1cdb8ee66822f1
SureshNarsingu <suri.sky08@gmail.com>
2022-07-14 14:56:43 +0530

    To find Prime number

---
+++ Prime.java
@@ -0,0 +1,18 @@
+class Prime{
+     public static void main(String[] args) {
+        
+        for(int i=100;i>=1;i--){
+            boolean flag=true;
+            for(int j=2;j<=i/2;j++){
+                if(i%j==0){
+                    flag=false;
+                    break;
+                }
+            }
+            if(flag==true){
+                System.out.println(i);
+                break;
+            }
+        }
+     }
+}
